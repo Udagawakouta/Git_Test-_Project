2f1e9f01b9e903ab9fa10b8d3be00636d6f944a3
Udagawakouta <115062885+Udagawakouta@users.noreply.github.com>
2022-12-15 23:41:07 +0900

    リモートリポジトリでコンフリクト変更

--- Git_Test _Project.cpp
+++ Git_Test _Project.cpp
@@ -7,7 +7,7 @@ int main()
 {
     std::cout << "Hello GitHub!\n";
 
-    std::cout << "Hello New Branch\n";
+    std::cout << "GitHub Clone changed\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
